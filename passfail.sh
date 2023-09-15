fbde8990dd0ea2221db7639d506835b65c71645b
Iamneo <techadmin@iamneo.ai>
2023-09-15 12:02:46 +0000

    Backup Commit

--- passfail.sh
+++ passfail.sh
@@ -1,7 +1,7 @@
 read -p "Enter First number: " num1
 read -p "Enter second number " num2
 sum=$((num1 + num2))
-if[ "$sum" -gt 70 ];
+if [ "$sum" -gt 70 ]; then
     echo "Pass"
 else
     echo "Fail"
